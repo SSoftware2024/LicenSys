e55039e6c0418ea313371be0e18dac1addfd1348
Soluções Software <ssolucoessoftware@gmail.com>
2025-02-26 20:55:50 -0300

    flowbite instalado

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { Link } from '@inertiajs/vue3';
-
+import 'flowbite';
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
