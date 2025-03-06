ecc075e83362e486919ca2751e0e683ced0a4aab
Soluções Software <ssolucoessoftware@gmail.com>
2025-03-06 02:29:44 -0300

    sidebar gradiente

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -1,8 +1,9 @@
 <template>
+    <!-- BUTTON SIDEBAR -->
     <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
+        data-drawer-target="sidebar-multi-level-sidebar"
+        data-drawer-toggle="sidebar-multi-level-sidebar"
+        aria-controls="sidebar-multi-level-sidebar"
         type="button"
         class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
     >
@@ -21,14 +22,17 @@
             ></path>
         </svg>
     </button>
+    <!-- END BUTTON SIDEBAR -->
+
+    <!-- SIDEBAR -->
 
     <aside
-        id="default-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full border-r-[0.2px] md:translate-x-0"
+        id="sidebar-multi-level-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
     >
         <div
-            class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800"
+            class="h-full px-3 py-4 overflow-y-auto bg-gradient-to-br from-zinc-100 to-gray-100 dark:bg-gray-800 dark:bg-none"
         >
             <ul class="space-y-2 font-medium">
                 <li>
@@ -54,6 +58,61 @@
                     </a>
                 </li>
                 <li>
+                    <button
+                        type="button"
+                        class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                        aria-controls="dropdown-example"
+                        data-collapse-toggle="dropdown-example"
+                    >
+                        <svg
+                            class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 18 21"
+                        >
+                            <path
+                                d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
+                            />
+                        </svg>
+                        <span
+                            class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap"
+                            >Usuários</span
+                        >
+                        <svg
+                            class="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 10 6"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="m1 1 4 4 4-4"
+                            />
+                        </svg>
+                    </button>
+                    <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                        <li>
+                            <a
+                                href="#"
+                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                >Comum</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                >Administrador</a
+                            >
+                        </li>
+                    </ul>
+                </li>
+                <li>
                     <a
                         href="#"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
@@ -70,7 +129,7 @@
                             />
                         </svg>
                         <span class="flex-1 ms-3 whitespace-nowrap"
-                            >Kanban</span
+                            >Empresas</span
                         >
                         <span
                             class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"
@@ -94,7 +153,9 @@
                                 d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"
                             />
                         </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
+                        <span class="flex-1 ms-3 whitespace-nowrap"
+                            >Grupo Empresa</span
+                        >
                         <span
                             class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
                             >3</span
@@ -117,13 +178,17 @@
                                 d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"
                             />
                         </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+                        <span class="flex-1 ms-3 whitespace-nowrap"
+                            >Sistema</span
+                        >
                     </a>
                 </li>
                 <li>
-                    <a
-                        href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    <button
+                        type="button"
+                        class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                        aria-controls="dropdown-examples"
+                        data-collapse-toggle="dropdown-examples"
                     >
                         <svg
                             class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -136,36 +201,44 @@
                                 d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"
                             />
                         </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap"
-                            >Products</span
+                        <span
+                            class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap"
+                            >Mensalidade</span
                         >
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                    >
                         <svg
-                            class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            class="w-3 h-3"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
-                            viewBox="0 0 18 16"
+                            viewBox="0 0 10 6"
                         >
                             <path
                                 stroke="currentColor"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
                                 stroke-width="2"
-                                d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                                d="m1 1 4 4 4-4"
                             />
                         </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap"
-                            >Sign In</span
-                        >
-                    </a>
+                    </button>
+                    <ul id="dropdown-examples" class="hidden py-2 space-y-2">
+                        <li>
+                            <a
+                                href="#"
+                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                >Histórico</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                >Receber</a
+                            >
+                        </li>
+                    </ul>
                 </li>
+
                 <li>
                     <a
                         href="#"
@@ -189,23 +262,59 @@
                             />
                         </svg>
                         <span class="flex-1 ms-3 whitespace-nowrap"
-                            >Sign Up</span
+                            >Perfil</span
                         >
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    >
+                        <svg
+                            class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 16"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                            />
+                        </svg>
+                        <span class="flex-1 ms-3 whitespace-nowrap">Sair</span>
                     </a>
                 </li>
             </ul>
         </div>
     </aside>
 
+    <!-- END SIDEBAR -->
+
+    <!-- CONTENT -->
     <div class="px-4 py-2 md:ml-64">
         <div class="container m-auto">
             <Card>
                 <slot></slot>
+                <button class="bg-red-400 p-2" @click="plus">
+                    More: +{{ number }}
+                </button>
             </Card>
         </div>
     </div>
+    <!-- END CONTENT -->
 </template>
 <script setup>
-import Card from '@/components/Card.vue';
+import Card from "@/components/Card.vue";
+import { ref } from "vue";
 
+const number = ref(0);
+
+const plus = () => {
+    number.value++;
+};
 </script>
