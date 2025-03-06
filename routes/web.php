393ade3d20b8d4c8ee6018234d15a0fed8381581
Soluções Software <ssolucoessoftware@gmail.com>
2025-03-06 11:03:09 -0300

    atualizaçoes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    ds('teste')->label('run')->success();
     return Inertia::render('Index');
 })->name('index');
 
