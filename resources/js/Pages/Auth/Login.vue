75d62dafe515f42b08f588c15d6123423e99d684
Soluções Software <ssolucoessoftware@gmail.com>
2025-03-07 16:48:31 -0300

    tela de login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -44,7 +44,8 @@
                     Lembrar de mim
                 </label>
             </div>
-            <div class="mt-1 flex justify-end">
+            <div class="mt-1 flex justify-between">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Esqueci a senha!</a>
                 <button
                     type="submit"
                     class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
