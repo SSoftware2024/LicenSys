4a4814491c6782e67ae4a0b327517ece56a4c986
Soluções Software <ssolucoessoftware@gmail.com>
2025-03-08 01:22:24 -0300

    laravel em pt-br

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return Inertia::render('Index');
 })->name('index');
 
