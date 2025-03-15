01f4fb85f4cef81c83bc1c66cd1fc3f8678e33a1
Soluções Software <ssolucoessoftware@gmail.com>
2025-03-15 17:10:42 -0300

    destacando rota atual

--- resources/js/Pages/System.vue
+++ resources/js/Pages/System.vue
@@ -1,15 +1,72 @@
 <template>
-    <Head title="Index"/>
-    <div>
-        System page
+    <Head title="Sistema" />
+    <h2 class="font-bold text-2xl underline mb-2">Sistema</h2>
+    <div class="xl:w-200">
+        <form @submit.prevent="">
+            <Input
+                type="number"
+                label="Limite de dias (expiração)"
+                id="number_days"
+                :isInputRequired="true"
+            />
+            <div class="flex sm:flex-wrap md:flex-nowrap">
+                <div class="flex flex-col w-full m-1">
+                    <Input
+                        type="text"
+                        label="Cód. Acesso API"
+                        id="code_access_api"
+                        :isInputRequired="true"
+                    />
+                </div>
+                <div class="flex flex-col w-full m-1">
+                    <Input
+                        type="text"
+                        label="Cód. Acesso API - Genéricos"
+                        id="code_access_api_generic"
+                        :isInputRequired="true"
+                    />
+                </div>
+            </div>
+            <div class="flex">
+                <div class="flex flex-col w-full m-1">
+                    <Input
+                        type="text"
+                        label="Cód. Acesso API - (Apenas leitura)"
+                        id="code_access_api_readonly"
+                        class="bg-gray-200"
+                        readonly
+                    />
+                </div>
+                <div class="flex flex-col w-full m-1">
+                    <Input
+                        type="text"
+                        label="Cód. Acesso API - Genéricos - (Apenas leitura)"
+                        id="code_access_api_generic_readonly"
+                        class="bg-gray-200"
+                        readonly
+                    />
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <Button text="Salvar" type="submit" typeButton='primary' :isDisable="form.processing" :isLoading="form.processing"></Button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { route } from "ziggy-js";
+import { useForm } from "@inertiajs/vue3";
+//COMPONENTS E LAYOUTS
 import SidebarLayout from "../layouts/SidebarLayout.vue";
+import Input from "@/components/Input.vue";
+import Button from "@/components/Button.vue";
 defineOptions({
     layout: SidebarLayout,
+});
+
+const form = useForm({
+    limit_days: 0
 });
 </script>
--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -1,5 +1,6 @@
 <template>
     <!-- BUTTON SIDEBAR -->
+    <!-- mude sidebar hide aqui: md:hidden, onde tem a classe de screen -->
     <button
         data-drawer-target="sidebar-multi-level-sidebar"
         data-drawer-toggle="sidebar-multi-level-sidebar"
@@ -26,9 +27,10 @@
 
     <!-- SIDEBAR -->
 
+    <!-- mude sidebar hide aqui: md:translate-x-0, onde tem a classe de screen  -->
     <aside
         id="sidebar-multi-level-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full md:translate-x-0"
         aria-label="Sidebar"
     >
         <div
@@ -38,10 +40,16 @@
                 <li>
                     <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                        :class="{
+                            'flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group': true,
+                            'text-lime-700': route().current('index'),
+                        }"
                     >
                         <svg
-                            class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            :class="{
+                                'w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white':true,
+                                'text-lime-700 group-hover:text-lime-900': route().current('index')
+                            }"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="currentColor"
@@ -62,10 +70,16 @@
                 <li>
                     <a
                         href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                        :class="{
+                            'flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group':true,
+                            'text-lime-700': route().current('system'),
+                        }"
                     >
                         <svg
-                            class="w-6 h-6 text-gray-800 dark:text-white"
+                            :class="{
+                                'w-6 h-6 text-gray-800 dark:text-white':true,
+                                'text-lime-700 group-hover:text-lime-900': route().current('system')
+                            }"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
