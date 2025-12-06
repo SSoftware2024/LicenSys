f9fe269b5c4ff08ba9aaea3cbee9e60aebe6c239
Soluções Software <ssolucoessoftware@gmail.com>
2025-12-06 13:06:05 -0300

    tela de erro e api rotas com sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Facades\Toast;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
 use App\Http\Middleware\Admin;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\VerfiyEmailAdmin;
+use App\Http\Middleware\HandleInertiaRequests;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\VerfiyEmailAdmin;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +27,18 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+            if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+                return Inertia::render('ErrorPage', ['status' => $response->getStatusCode()])
+                    ->toResponse($request)
+                    ->setStatusCode($response->getStatusCode());
+            }
+
+            if ($response->getStatusCode() === 419) {
+                Toast::error('Sessão expirada! Por favor, tente novamente.');
+                return back()->withInput();
+            }
+
+            return $response;
+        });
     })->create();
--- config/app.php
+++ config/app.php
@@ -23,10 +23,10 @@ return [
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
     | services the application utilizes. Set this in your ".env" file.
-    |
+    | 'production','local','staging'
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:k0K73YZyn4NMqipub12HnD14giSNAztYGXDzhhkdYho='),
 
     'previous_keys' => [
         ...array_filter(
