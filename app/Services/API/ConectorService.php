c956eecb5c2320ce8812db61324d3bf704a3d60e
Soluções Software <ssolucoessoftware@gmail.com>
2026-01-27 18:17:23 -0300

    recebendo requisiçoes

--- app/Services/API/ConectorService.php
+++ app/Services/API/ConectorService.php
@@ -4,7 +4,6 @@ namespace App\Services\API;
 final class ConectorService
 {
     public function connect(){
-        // return response()->json(['message' => 'ConectorService is working!']);
-        return json_encode(['status' => 'success', 'data' => []]);
+        return response()->json(['message' => 'ConectorService is working!']);
     }
 }
